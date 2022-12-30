2bc1c19d5e2538bceaff4e9150cdac4d3746c5fa
Partha sarathi <parthasahoo2677@gmail.com>
2022-12-30 12:10:17 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+     public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name = "PARTHA";
+//         java: name has private access in com.driver.RWOnly
+         obj.setName("PARTHA");
+         System.out.println(obj.getName());
+    }
   
 }
